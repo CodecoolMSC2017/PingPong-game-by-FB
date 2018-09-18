7275646b3c8c507c9672a7b275cec38ca4143dbb
Farago Bence <semmiertelme13@gmail.com>
2018-09-18 11:21:13 +0200

    Created the basic game

--- PingPongGame/PingPongGame/Form1.Designer.cs
+++ PingPongGame/PingPongGame/Form1.Designer.cs
@@ -29,12 +29,79 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.Racket = new System.Windows.Forms.PictureBox();
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.PlayerScore = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Racket)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Racket
+            // 
+            this.Racket.BackColor = System.Drawing.Color.DarkRed;
+            this.Racket.Location = new System.Drawing.Point(280, 454);
+            this.Racket.Name = "Racket";
+            this.Racket.Size = new System.Drawing.Size(364, 50);
+            this.Racket.TabIndex = 0;
+            this.Racket.TabStop = false;
+            // 
+            // Ball
+            // 
+            this.Ball.BackColor = System.Drawing.Color.Black;
+            this.Ball.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Ball.Location = new System.Drawing.Point(412, 327);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(55, 55);
+            this.Ball.TabIndex = 1;
+            this.Ball.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // PlayerScore
+            // 
+            this.PlayerScore.AutoSize = true;
+            this.PlayerScore.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.PlayerScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.PlayerScore.ForeColor = System.Drawing.Color.Red;
+            this.PlayerScore.Location = new System.Drawing.Point(30, 539);
+            this.PlayerScore.Name = "PlayerScore";
+            this.PlayerScore.Size = new System.Drawing.Size(36, 26);
+            this.PlayerScore.TabIndex = 2;
+            this.PlayerScore.Text = "00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(928, 574);
+            this.Controls.Add(this.PlayerScore);
+            this.Controls.Add(this.Ball);
+            this.Controls.Add(this.Racket);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Racket)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox Racket;
+        private System.Windows.Forms.PictureBox Ball;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label PlayerScore;
     }
 }
 
--- PingPongGame/PingPongGame/Form1.cs
+++ PingPongGame/PingPongGame/Form1.cs
@@ -12,9 +12,111 @@ namespace PingPongGame
 {
     public partial class Form1 : Form
     {
+
+        bool goLeft;
+        bool goRight;
+        int speed = 5;
+        int ballx = 5;
+        int bally = 5;
+        int score = 0;
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public Timer t;
+
+        private void t_Tick(object sender, EventArgs e)
+        {
+
+            if (Ball.Location.Y > 0 && Ball.Location.Y < 489){
+                Ball.Location = new Point(
+                Ball.Location.X + 10,
+                Ball.Location.Y + 10);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            PlayerScore.Text = "" + score;
+            Ball.Top -= bally;
+            Ball.Left -= ballx;
+
+            Console.WriteLine("Client:" + ClientSize.Width);
+            Console.WriteLine("Ball:" +(Ball.Right + Ball.Width));
+
+
+            if (Ball.Top < 0)
+            {
+
+                bally = -bally;
+                score++;
+                
+            }
+            if(Ball.Top - Ball.Height > ClientSize.Height)
+            {
+                Ball.Top = ClientSize.Height/2;
+                Ball.Left = ClientSize.Width/2;
+                bally = -bally;
+                // MessageBox.Show("You lost!");
+            }
+
+            if (Ball.Left + Ball.Width-50  < 0)
+            {
+                ballx = -ballx;
+            }
+            if(Ball.Left + Ball.Width > ClientSize.Width)
+            {
+                ballx = -ballx;
+            }
+            if (Ball.Bounds.IntersectsWith(Racket.Bounds))
+            {
+                bally = -bally;
+            }
+
+            if(goLeft == true && Racket.Left > 0)
+            {
+                Racket.Left -= 8;
+            }
+            if (goRight == true && Racket.Left + Racket.Width < 925)
+            {
+                Racket.Left += 8;
+            }
+
+
         }
     }
 }
